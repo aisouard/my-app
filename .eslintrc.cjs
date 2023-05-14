41731d7c08263fe98df62d6fd1bdd0d352c2ea0a
Axel Isouard <axel@isouard.fr>
2023-05-14 17:11:56 +0200

    Storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,28 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:import/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
-  ],
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:import/recommended', 'plugin:jsx-a11y/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   plugins: ['react-refresh'],
   settings: {
     react: {
-      version: 'detect',
+      version: 'detect'
     },
     'import/resolver': {
       node: {
         paths: ['src', 'public'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
   },
   rules: {
     'prettier/prettier': 'warn',
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+    'react-refresh/only-export-components': 'warn'
+  }
+};
--- src/App.cy.tsx
+++ src/App.cy.tsx
@@ -1,9 +1,9 @@
-import App from './App'
+import App from "./App";
 
-describe('<App />', () => {
-  it('displays initial counter and increments upon click', () => {
-    cy.mount(<App />)
-    cy.get('button').should('have.text', 'count is 0')
-    cy.get('button').click().should('have.text', 'count is 1')
-  })
-})
+describe("<App />", () => {
+  it("displays initial counter and increments upon click", () => {
+    cy.mount(<App />);
+    cy.get("button").should("have.text", "count is 0");
+    cy.get("button").click().should("have.text", "count is 1");
+  });
+});
