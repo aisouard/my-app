bc13673538c085f207c1cb9aaee68671db6ff61e
Axel Isouard <axel@isouard.fr>
2023-05-14 17:09:32 +0200

    Cypress component

---
+++ src/App.cy.tsx
@@ -0,0 +1,9 @@
+import App from './App'
+
+describe('<App />', () => {
+  it('displays initial counter and increments upon click', () => {
+    cy.mount(<App />)
+    cy.get('button').should('have.text', 'count is 0')
+    cy.get('button').click().should('have.text', 'count is 1')
+  })
+})
